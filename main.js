d849da2b18931953be63f7a4f446178ee1538d24
Katikalockers <saipan1150@gmail.com>
2026-07-06 00:46:23 +0700

    5.5*

--- main.js
+++ main.js
@@ -9,6 +9,19 @@ createApp({
         const inStock = ref(true)
         const inventory = ref(9)
         const onSale = ref(true)
+
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg'}
+        ])
+        const sizes = ref([
+            'S','M','L'
+        ])
         return {
             product,
             description,
@@ -16,7 +29,10 @@ createApp({
             link,
             inStock,
             inventory,
-            onSale
+            onSale,
+            details,
+            variants,
+            sizes
         }
     }
 
