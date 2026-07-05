05e9f1d3115b6aff298be0b86c6cc26cc8a2ec51
Katikalockers <saipan1150@gmail.com>
2026-07-06 00:25:07 +0700

    3.2

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ createApp({
     setup(){
         const product = ref('Boots')
         const description = ref('Description: good')
+        const image = ref('./assets/images/socks_green.jpg')
         return {
             product,
-            description
+            description,
+            image
         }
     }
 
