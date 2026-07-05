66eac3df71607adeb51f328d4b5e214de9c390e4
Katikalockers <saipan1150@gmail.com>
2026-07-06 00:28:09 +0700

    3.6*

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ createApp({
         const product = ref('Boots')
         const description = ref('Description: good')
         const image = ref('./assets/images/socks_green.jpg')
+        const link = ref('http://www.camt.cmu.ac.th')
         return {
             product,
             description,
-            image
+            image,
+            link
         }
     }
 
