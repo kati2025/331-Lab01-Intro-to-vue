79ca852d565fc07ad7acda4d6bea7346bb6b24cf
Katikalockers <saipan1150@gmail.com>
2026-07-06 03:47:01 +0700

    8.3

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
+        const brand = ref('SE 331')
         const description = ref('Description: good')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('http://www.camt.cmu.ac.th')
@@ -16,9 +17,10 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg'}
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg', quantity: 0}
         ])
+        const selectedVariant = ref(0)
         const sizes = ref([
             'S','M','L'
         ])
@@ -27,14 +29,27 @@ createApp({
             function addToCart() {
                 cart.value +=1
             }
+        const title = computed(() =>{
+            return brand.value + ' ' + product.value
+        })
             function updateImage(variantImage){
                 image.value = variantImage
             }
             function toggleStock() {
                 inStock.value =!inStock.value
             }
+            function updateVariant(index){
+                selectedVariant.value = index;
+            }
+            const image = computed(() => {
+                return variants.value[selectedVariant.value].image
+            })
+            const inStock = computed(() => {
+                return variants.value[selectedVariant.value].quantity
+            })
+
         return {
-            product,
+            title,
             description,
             image,
             link,
